b0702f1ecd9026085b50a6d584980fa64f7418c6
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-06-09 16:39:10 -0700

    added buttons and some css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className='fighters'>
+        <p>🐍</p>
+        <button>Attack1!</button>
+        <button>Attack2</button>
+      </div>
+      <div className='fighters'>
+        <p>🦜</p>
+        <button>Attack3</button>
+        <button>Attack4</button>
+      </div>
+
+
+
+
+
+
+
+
     </div>
   );
 }
